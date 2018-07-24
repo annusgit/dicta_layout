632e6156b15780939e603fe5b55cfcf39588acbf
annusgit <annusgit@gmail.com>
2018-07-24 17:06:41 +0200

    cutting now

--- hocr.py
+++ hocr.py
@@ -5,6 +5,7 @@ from __future__ import division
 import os
 import cv2
 import sys
+import numpy as np
 from tqdm import trange
 import xml.etree.ElementTree as et
 
@@ -16,32 +17,36 @@ xmls = [xml for xml in os.listdir(xml_source) if xml.endswith('.txt')]
 os.mkdir(dest)
 
 
-for k in trange(len(images)):
-    file_name, ext = os.path.splitext(images[k])
-    root = et.parse(os.path.join(xml_source, file_name+'.txt')).getroot()
-    img = cv2.imread(os.path.join(img_source, file_name+'.jpg'))
+# for k in trange(len(images)):
+name = '00001139.jpg'
+file_name, ext = os.path.splitext(name)
+root = et.parse(os.path.join(xml_source, file_name+'.txt')).getroot()
+img = cv2.imread(os.path.join(img_source, file_name+'.jpg'))
 
-    tags = []
-    bbox = []
-    def recurse(root):
-        for child in root:
-            recurse(child)
-        class_ = root.get('class')
-        if class_ == 'ocr_par' or class_ == 'ocr_carea':
-            bbox_line = root.get('title').split(';')[0].split(' ')[1:]
-            bbox.append(map(int, bbox_line))
-        if not class_ in tags:
-            tags.append(class_)
+tags = []
+bbox = []
+def recurse(root):
+    for child in root:
+        recurse(child)
+    class_ = root.get('class')
+    if class_ == 'ocr_par' or class_ == 'ocr_carea':
+        bbox_line = root.get('title').split(';')[0].split(' ')[1:]
+        bbox.append(map(int, bbox_line))
+    if not class_ in tags:
+        tags.append(class_)
 
-    recurse(root)
-    # print(tags)
-    # print(bbox)
-    for line in bbox:
-        x1, y1, x2, y2 = line
-        # at this point, we have bo
-        cv2.rectangle(img, (x1, y1), (x2, y2), (0, 0, 255), 2)
-    # img = cv2.flip(img, 0)
-    cv2.imwrite(os.path.join(dest, '{}.png'.format(file_name)), img)
+recurse(root)
+# print(tags)
+# print(bbox)
+img = cv2.flip(img, 0)
+for idx, line in enumerate(bbox):
+    x1, y1, x2, y2 = line
+    new = np.zeros_like(img)
+    new[x1:x2,y1:y2,:] = img[x1:x2,y1:y2,:]
+    # at this point, we have bounding boxes of entities
+    # cv2.rectangle(img, (x1, y1), (x2, y2), (0, 0, 255), 2)
+    new = cv2.flip(new, 0)
+    cv2.imwrite(os.path.join(dest, '{}_{}.png'.format(file_name, idx)), new)
 
 
 
